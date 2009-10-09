5e2d7b36420215cdf1cd040560e107ba2bd0cdd4
Geoffrey Longman <glongman@turbonium2.lan>
2009-10-09 03:12:06 -0400

    simpler and cleaner

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,6 +1,20 @@
 class CallsController < ApplicationController
-  def index; response.headers['type'] = 'application/json' end #TODO mondo crappo
-  def new; response.headers['type'] = 'application/json' end
+  
+  def index
+    render :text => [
+      {:name => :Speak, :phrase => 'Welcome to ShopTalk. Enter your 4 digit access code.'},
+      {:name => :GetDigits, :url => "#{new_call_url}.json", :max => 4},
+      {:name => :playback, :filename => 'airport'},
+      {:name => :playback, :filename => 'north-dakota'}
+    ].to_json
+  end 
+  
+  def new
+    render :text => [
+    { :name => :Speak, :phrase => "You entered these digits. #{params[:result]}"},
+    { :name => :Speak, :phrase => "You call id is. #{params[:call_id]}"},
+    ].to_json
+  end
   def create; end
   def destroy; end
   # no update
