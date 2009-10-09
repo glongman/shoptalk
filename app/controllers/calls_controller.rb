7e56d1346bbebe194313cdf447d2afe03c09169b
Geoffrey Longman <glongman@turbonium2.lan>
2009-10-09 02:34:05 -0400

    hacking content types

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,6 +1,6 @@
 class CallsController < ApplicationController
-  def index; end
-  def new; end
+  def index; response.headers['type'] = 'application/json' end #TODO mondo crappo
+  def new; response.headers['type'] = 'application/json' end
   def create; end
   def destroy; end
   # no update
